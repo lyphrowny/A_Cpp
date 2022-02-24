2bc64ff943f715b2ca1947f78f49e5568b5ea632
lyphrowny <strukov.as@edu.spbstu.ru>
2022-02-25 00:13:48 +0300

    removed unnecessary comments, added error handling

--- operation/Operation.h
+++ operation/Operation.h
@@ -29,11 +29,11 @@ public:
                                             _arity(arity),
                                             _func(std::move(func)) {}
 
-    int getPrecedence() { return this->_precedence; }
+    int getPrecedence() const { return this->_precedence; }
 
     std::string& getName() { return this->_name; }
 
     Arity& getArity() { return this->_arity; }
 
-    double calcFunc(std::vector<double> args) { return this->_func(args); }
+    double calcFunc(const std::vector<double>& args) { return this->_func(args); }
 };
